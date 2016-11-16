cbb1cfac02b111e5b2dd5cce4a8080c1cb58f704
DESKTOP-J9LBAEI\petly <petliak.serhii@gmail.com>
2016-11-16 17:46:18 +0200

    add box blocks

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -8,24 +8,27 @@
     <div class="container-fluid spark-screen">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                <form action="" method="post">
-                    {{csrf_field()}}
-                    <div class="form-group has-feedback">
-                        <input type="text" class="form-control" placeholder="Имя" name="name" value="{{ old('name') }}"/>
-                        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                <div class="box box-primary">
+                    <div class="box-body">
+                        <form action="" method="post">
+                            {{csrf_field()}}
+                            <div class="form-group has-feedback">
+                                <input type="text" class="form-control" placeholder="Имя" name="name" value="{{ old('name') }}"/>
+                                <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                            </div>
+                            <div class="form-group has-feedback">
+                                <input type="email" class="form-control" placeholder="Ел. почта}" name="email" value="{{ old('email') }}"/>
+                                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                            </div>
+                            <div class="form-group has-feedback">
+                                <input type="password" class="form-control" placeholder="Пароль" name="password"/>
+                                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                            </div>
+                            <button type="submit" class="btn btn-primary btn-flat">Зарегистрировать</button>
+                        </form>
                     </div>
-                    <div class="form-group has-feedback">
-                        <input type="email" class="form-control" placeholder="Ел. почта}" name="email" value="{{ old('email') }}"/>
-                        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-                    </div>
-                    <div class="form-group has-feedback">
-                        <input type="password" class="form-control" placeholder="Пароль" name="password"/>
-                        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                    </div>
-                    <button type="submit" class="btn btn-primary btn-flat">Зарегистрировать</button>
-                </form>
+                </div>
             </div>
-
         </div>
     </div>
 @endsection
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -7,32 +7,38 @@
 @section('main-content')
     <div class="container-fluid spark-screen">
         <div class="row">
-            <a href="{!! route("user.create") !!}" class="btn btn-success btn-flat add__button">Добавить</a>
-            <table class="table table-bordered table-striped">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Имя</th>
-                        <th>Дата создания</th>
-                        <th>Действия</th>
-                    </tr>
-                </thead>
-                @foreach($data as $item)
-                    <tr>
-                        <td>{{ $item->id }}</td>
-                        <td>{{ $item->name }}</td>
-                        <td>{{ $item->created_at }}</td>
-                        <td>
-                            <a href="{{ route('user.edit',['id'=>$item['id']]) }}"
-                               class="btn btn-default" title="Update"><span class="glyphicon glyphicon-pencil"></span></a>
-                            <a href="{{ route('user.delete',['id'=>$item['id']]) }}"
-                               onclick="return confirm('Удалить выбраную запись?')"
-                               class="btn btn-danger" title="Delete"><span class="glyphicon glyphicon-trash"></span></a>
-                        </td>
-                    </tr>
-                @endforeach
-            </table>
-            {!! $data->links() !!}
+            <div class="box box-primary">
+                <div class="box-body">
+                    <a href="{!! route("user.create") !!}" class="btn btn-success btn-flat add__button">Добавить</a>
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Имя</th>
+                                <th>Дата создания</th>
+                                <th>Действия</th>
+                            </tr>
+                        </thead>
+                        @foreach($data as $item)
+                            <tr>
+                                <td>{{ $item->id }}</td>
+                                <td>{{ $item->name }}</td>
+                                <td>{{ $item->created_at }}</td>
+                                <td>
+                                    <a href="{{ route('user.edit',['id'=>$item['id']]) }}"
+                                       class="btn btn-default" title="Update"><span class="glyphicon glyphicon-pencil"></span></a>
+                                    <a href="{{ route('user.delete',['id'=>$item['id']]) }}"
+                                       onclick="return confirm('Удалить выбраную запись?')"
+                                       class="btn btn-danger" title="Delete"><span class="glyphicon glyphicon-trash"></span></a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </table>
+                </div>
+                <div class="box-footer text-center">
+                    {!! $data->links() !!}
+                </div>
+            </div>
         </div>
     </div>
 @endsection
