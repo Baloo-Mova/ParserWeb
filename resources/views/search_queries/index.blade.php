715afde302b274561a39ffce3cf7ac18575bbbc3
DESKTOP-J9LBAEI\petly <petliak.serhii@gmail.com>
2016-11-21 20:02:12 +0200

    add modal for csv export in search-queries

--- resources/views/search_queries/index.blade.php
+++ resources/views/search_queries/index.blade.php
@@ -9,7 +9,11 @@
         <div class="row">
             <div class="box box-primary">
                 <div class="box-body">
-                    <a href="#" class="btn btn-success btn-flat pull-left add__button">Сохранить В CSV</a>
+                    <button type="button" class="btn btn-success btn-flat pull-left add__button" data-toggle="modal" data-target="#searchQueryModal">
+                        Сохранить В CSV
+                    </button>
+
+
                     <table class="table table-bordered table-striped">
                         <thead>
                             <tr>
@@ -51,6 +55,40 @@
                     {!! $data->links() !!}
                 </div>
                 <div class="box-footer text-center">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade searchQueryModal" id="searchQueryModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel"> Создать Выборку и Сохранить В CSV</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group has-feedback">
+                        <label for="login" class="control-label">Страна</label>
+                        <select name="" id="" class="form-control">
+
+                        </select>
+                    </div>
+                    <div class="form-group has-feedback">
+                        <label for="login" class="control-label">Город</label>
+                        <select name="" id="" class="form-control">
+
+                        </select>
+                    </div>
+                    <div class="form-group has-feedback">
+                        <label for="login" class="control-label">Поисковый Запрос</label>
+                        <input type="text" class="form-control">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal">Закрыть</button>
+                    <button type="button" class="btn btn-primary btn-flat pull-left">Сохранить</button>
                 </div>
             </div>
         </div>
