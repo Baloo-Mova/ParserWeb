eaaf76eaf24a8e4ed20c2ae2264a9ec1f4233856
DESKTOP-J9LBAEI\petly <petliak.serhii@gmail.com>
2016-11-26 15:40:01 +0200

    update tasks type model

--- app/Models/TasksType.php
+++ app/Models/TasksType.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class TasksType extends Model
 {
+    const WORD    = 1;
+    const SITES    = 2;
+
     public $timestamps = false;
     public $table = "tasks_type";
 
