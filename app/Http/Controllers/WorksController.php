152157141c4dd344eb0937648592f70d1114dc21
DESKTOP-J9LBAEI\petly <petliak.serhii@gmail.com>
2016-11-28 22:51:54 +0200

    add supervisor xmlrpc api

--- app/Http/Controllers/WorksController.php
+++ app/Http/Controllers/WorksController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Supervisor\Api;
 
 
 
 class WorksController extends Controller
 {
-    public function startWork($name)
+    public function index()
     {
+        $api = new Api('127.0.0.1', port, 'login', 'pass' );
 
+        // Call Supervisor API
+        dd($api->getProcessInfo('myworker'));
     }
 }
