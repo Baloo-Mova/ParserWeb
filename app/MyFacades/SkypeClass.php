1ec542a774c5cbaf6d169a51592bc508ec96c1b9
VlaD77 <ternovoyvladislav@mail.ru>
2017-03-31 21:36:42 +0300

    SkypeClass: corrected the sends of messages and Invitations to friends

--- app/MyFacades/SkypeClass.php
+++ app/MyFacades/SkypeClass.php
@@ -5,29 +5,25 @@ namespace App\MyFacades;
 use App\Models\SkypeLogins;
 use App\Models\Parser\ErrorLog;
 
-class SkypeClass
-{
+class SkypeClass {
 
-    public $username, $valid                                              = true;
-    private           $password, $registrationToken, $skypeToken, $expiry = 0, $logged = false, $hashedUsername;
+    public $username, $valid = true;
+    private $password, $registrationToken, $skypeToken, $expiry = 0, $logged = false, $hashedUsername;
 
-    public function addSender($username, $password)
-    {
+    public function addSender($username, $password) {
         $this->username = $username;
         $this->password = $password;
-        $this->valid    = $this->login();
+        $this->valid = $this->login();
     }
 
-    private function login()
-    {
+    private function login() {
         try {
 
             $this->registrationToken = null;
             $this->expiry = 0;
             $this->skypeToken = null;
 
-            $loginForm = $this->web("https://login.skype.com/login/oauth/microsoft?client_id=578134&redirect_uri=https%3A%2F%2Fweb.skype.com%2F&username={$this->username}",
-                "GET", [], true, true);
+            $loginForm = $this->web("https://login.skype.com/login/oauth/microsoft?client_id=578134&redirect_uri=https%3A%2F%2Fweb.skype.com%2F&username={$this->username}", "GET", [], true, true);
 
             preg_match("`urlPost:'(.+)',`isU", $loginForm, $loginURL);
             $loginURL = $loginURL[1];
@@ -45,23 +41,23 @@ class SkypeClass
             }
 
             $post = [
-                "loginfmt"     => $this->username,
-                "login"        => $this->username,
-                "passwd"       => $this->password,
-                "type"         => 11,
-                "PPFT"         => $ppft,
-                "PPSX"         => $ppsx,
-                "NewUser"      => (int)1,
+                "loginfmt" => $this->username,
+                "login" => $this->username,
+                "passwd" => $this->password,
+                "type" => 11,
+                "PPFT" => $ppft,
+                "PPSX" => $ppsx,
+                "NewUser" => (int) 1,
                 "LoginOptions" => 3,
-                "FoundMSAs"    => "",
-                "fspost"       => (int)0,
-                "i2"           => (int)1,
-                "i16"          => "",
-                "i17"          => (int)0,
-                "i18"          => "__DefaultLoginStrings|1,__DefaultLogin_Core|1,",
-                "i19"          => 556374,
-                "i21"          => (int)0,
-                "i13"          => (int)0
+                "FoundMSAs" => "",
+                "fspost" => (int) 0,
+                "i2" => (int) 1,
+                "i16" => "",
+                "i17" => (int) 0,
+                "i18" => "__DefaultLoginStrings|1,__DefaultLogin_Core|1,",
+                "i19" => 556374,
+                "i21" => (int) 0,
+                "i13" => (int) 0
             ];
 
             $loginForm = $this->web($loginURL, "POST", $post, true, true, $cookies);
@@ -71,8 +67,8 @@ class SkypeClass
             preg_match("`<input type=\"hidden\" name=\"t\" id=\"t\" value=\"(.+)\">`isU", $loginForm, $t);
 
             $validskype = SkypeLogins::where(['login' => $this->username])->first();
-            if ( ! isset($NAP[1]) || ! isset($ANON[1]) || ! isset($t[1])) {
-                if ( ! (empty($validskype))) {
+            if (!isset($NAP[1]) || !isset($ANON[1]) || !isset($t[1])) {
+                if (!(empty($validskype))) {
                     $validskype->valid = 0;
                     $validskype->save();
                 }
@@ -80,13 +76,13 @@ class SkypeClass
                 return false;
                 //exit(trigger_error("Skype : Authentication failed for {$this->username}", E_USER_WARNING));
             }
-            if ( ! (empty($validskype))) {
+            if (!(empty($validskype))) {
                 $validskype->valid = 1;
                 $validskype->save();
             }
-            $NAP  = $NAP[1];
+            $NAP = $NAP[1];
             $ANON = $ANON[1];
-            $t    = $t[1];
+            $t = $t[1];
 
             preg_match_all('`Set-Cookie: (.+)=(.+);`isU', $loginForm, $cookiesArray);
             $cookies = "";
@@ -95,35 +91,32 @@ class SkypeClass
             }
 
             $post = [
-                "NAP"  => $NAP,
+                "NAP" => $NAP,
                 "ANON" => $ANON,
-                "t"    => $t
+                "t" => $t
             ];
 
-            $loginForm = $this->web("https://lw.skype.com/login/oauth/proxy?client_id=578134&redirect_uri=https://web.skype.com/&site_name=lw.skype.com&wa=wsignin1.0",
-                "POST", $post, true, true, $cookies);
+            $loginForm = $this->web("https://lw.skype.com/login/oauth/proxy?client_id=578134&redirect_uri=https://web.skype.com/&site_name=lw.skype.com&wa=wsignin1.0", "POST", $post, true, true, $cookies);
 
             preg_match("`<input type=\"hidden\" name=\"t\" value=\"(.+)\"/>`isU", $loginForm, $t);
             $t = $t[1];
 
             $post = [
-                "t"            => $t,
-                "site_name"    => "lw.skype.com",
+                "t" => $t,
+                "site_name" => "lw.skype.com",
                 "oauthPartner" => 999,
-                "form"         => "",
-                "client_id"    => 578134,
+                "form" => "",
+                "client_id" => 578134,
                 "redirect_uri" => "https://web.skype.com/"
             ];
 
-            $login = $this->web("https://login.skype.com/login/microsoft?client_id=578134&redirect_uri=https://web.skype.com/",
-                "POST", $post);
+            $login = $this->web("https://login.skype.com/login/microsoft?client_id=578134&redirect_uri=https://web.skype.com/", "POST", $post);
 
             preg_match("`<input type=\"hidden\" name=\"skypetoken\" value=\"(.+)\"/>`isU", $login, $skypeToken);
 
             $this->skypeToken = $skypeToken[1];
 
-            $login = $this->web("https://client-s.gateway.messenger.live.com/v1/users/ME/endpoints", "POST", "{}",
-                true);
+            $login = $this->web("https://client-s.gateway.messenger.live.com/v1/users/ME/endpoints", "POST", "{}", true);
 
             preg_match("`registrationToken=(.+);`isU", $login, $registrationToken);
 
@@ -138,20 +131,20 @@ class SkypeClass
 
             if (empty($skype_logins)) {
                 unset($skype_logins);
-                $skype_logins           = new SkypeLogins();
-                $skype_logins->login    = $this->username;
+                $skype_logins = new SkypeLogins();
+                $skype_logins->login = $this->username;
                 $skype_logins->password = $this->password;
             }
 
-            $skype_logins->skypeToken        = $this->skypeToken;
+            $skype_logins->skypeToken = $this->skypeToken;
             $skype_logins->registrationToken = $this->registrationToken;
-            $skype_logins->expiry            = $this->expiry;
-            $skype_logins->valid             = 1;
+            $skype_logins->expiry = $this->expiry;
+            $skype_logins->valid = 1;
             $skype_logins->save();
 
             return true;
         } catch (\Exception $ex) {
-            $log          = new ErrorLog();
+            $log = new ErrorLog();
             $log->message = $ex->getTraceAsString();
             $log->task_id = 0;
             $log->save();
@@ -160,19 +153,13 @@ class SkypeClass
     }
 
     private function web(
-        $url,
-        $mode = "GET",
-        $post = [],
-        $showHeaders = false,
-        $follow = true,
-        $customCookies = "",
-        $customHeaders = []
+    $url, $mode = "GET", $post = [], $showHeaders = false, $follow = true, $customCookies = "", $customHeaders = []
     ) {
-        if ( ! function_exists("curl_init")) {
+        if (!function_exists("curl_init")) {
             exit(trigger_error("Skype : cURL is required", E_USER_WARNING));
         }
 
-        if ( ! empty($post) && is_array($post)) {
+        if (!empty($post) && is_array($post)) {
             $post = http_build_query($post);
         }
 
@@ -194,11 +181,11 @@ class SkypeClass
         $curl = curl_init();
 
         curl_setopt($curl, CURLOPT_URL, $url);
-        if ( ! empty($headers)) {
+        if (!empty($headers)) {
             curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
         }
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $mode);
-        if ( ! empty($post)) {
+        if (!empty($post)) {
             curl_setopt($curl, CURLOPT_POSTFIELDS, $post);
         }
         if ($customCookies) {
@@ -206,8 +193,7 @@ class SkypeClass
         }
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($curl, CURLOPT_USERAGENT,
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36");
+        curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36");
         curl_setopt($curl, CURLOPT_HEADER, $showHeaders);
         curl_setopt($curl, CURLOPT_FOLLOWLOCATION, $follow);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -218,9 +204,8 @@ class SkypeClass
         return $result;
     }
 
-    public function logout()
-    {
-        if ( ! $this->logged) {
+    public function logout() {
+        if (!$this->logged) {
             return true;
         }
 
@@ -232,61 +217,57 @@ class SkypeClass
         return true;
     }
 
-    public function sendFriendInvite($from, $to, $message)
-    {
+    public function sendFriendInvite($from, $to, $message) {
 
         $this->index($from["login"], $from["password"]);
 
         $username = $this->URLtoUser($to);
-        $post     = [
+        $post = [
             "greeting" => $message
         ];
 
-        $req  = $this->web("https://api.skype.com/users/self/contacts/auth-request/$to", "PUT", $post);
+        $req = $this->web("https://api.skype.com/users/self/contacts/auth-request/$to", "PUT", $post);
         $data = json_decode($req, true);
 
         return isset($data["code"]) && $data["code"] == 20100;
     }
 
-    public function index($username, $password)
-    {
+    public function index($username, $password) {
 
-        $this->username       = $username;
-        $this->password       = $password;
+        $this->username = $username;
+        $this->password = $password;
         $this->hashedUsername = sha1($username);
-        $needRelogin          = false;
-        $skype_logins         = SkypeLogins::where('login', $username)->first();
+        $needRelogin = false;
+        $skype_logins = SkypeLogins::where('login', $username)->first();
 
-        if ( ! (empty($skype_logins))) {
-            $auth['skypeToken']        = $skype_logins->skypeToken;
+        if (!(empty($skype_logins))) {
+            $auth['skypeToken'] = $skype_logins->skypeToken;
             $auth['registrationToken'] = $skype_logins->registrationToken;
-            $auth['expiry']            = $skype_logins->expiry;
+            $auth['expiry'] = $skype_logins->expiry;
             if (time() >= $auth["expiry"]) {
                 $needRelogin = true;
             }
         }
 
-        if ( ! $needRelogin) {
-            $this->skypeToken        = $auth["skypeToken"];
+        if (!$needRelogin) {
+            $this->skypeToken = $auth["skypeToken"];
             $this->registrationToken = $auth["registrationToken"];
-            $this->expiry            = $auth["expiry"];
-            $this->valid             = true;
+            $this->expiry = $auth["expiry"];
+            $this->valid = true;
         } else {
             $this->valid = $this->login();
         }
     }
 
-    private function URLToUser($url)
-    {
+    private function URLToUser($url) {
         $url = explode(":", $url, 2);
 
         return end($url);
     }
 
-    /* New methods*/
+    /* New methods */
 
-    public function sendFrom($from, $to, $message)
-    {
+    public function sendFrom($from, $to, $message) {
 
         $this->index($from["login"], $from["password"]);
 
@@ -302,50 +283,45 @@ class SkypeClass
         }
     }
 
-    public function isFriend($to, $message)
-    {
+    public function isFriend($to, $message) {
 
         $post = [
             "greeting" => $message
         ];
 
-        $req  = $this->web("https://api.skype.com/users/self/contacts/auth-request/$to", "PUT", $post);
+        $req = $this->web("https://api.skype.com/users/self/contacts/auth-request/$to", "PUT", $post);
         $data = json_decode($req, true);
 
         return $data['status']['code'];
     }
 
-    public function sendMessage($from, $to, $message)
-    {
+    public function sendMessage($from, $to, $message) {
         $this->index($from["login"], $from["password"]);
 
-        $user      = $this->URLtoUser($to);
-        $mode      = strstr($user, "thread.skype") ? 19 : 8;
+        $user = $this->URLtoUser($to);
+        $mode = strstr($user, "thread.skype") ? 19 : 8;
         $messageID = $this->timestamp();
-        $post      = [
-            "content"         => $message,
-            "messagetype"     => "RichText",
-            "contenttype"     => "text",
+        $post = [
+            "content" => $message,
+            "messagetype" => "RichText",
+            "contenttype" => "text",
             "clientmessageid" => $messageID
         ];
 
-        $req = json_decode($this->web("https://client-s.gateway.messenger.live.com/v1/users/ME/conversations/$mode:$to/messages",
-            "POST", json_encode($post)), true);
+        $req = json_decode($this->web("https://client-s.gateway.messenger.live.com/v1/users/ME/conversations/$mode:$to/messages", "POST", json_encode($post)), true);
 
         return isset($req["OriginalArrivalTime"]) ? $messageID : 0;
     }
 
-    private function timestamp()
-    {
+    private function timestamp() {
         return str_replace(".", "", microtime(1));
     }
 
-    public function sendRandom($to, $message)
-    {
+    public function sendRandom($to, $message) {
         while (true) {
             $from = SkypeLogins::where(['valid' => '1'])->orderByRaw('RAND()')->first();
 
-            if ( ! empty($from)) {
+            if (!empty($from)) {
                 $this->index($from->login, $from->password);
 
                 if ($this->valid == false) {
@@ -353,22 +329,65 @@ class SkypeClass
                     continue;
                 }
 
-                $is_friend = $this->isFriend($to, $message);
+                $is_friend = $this->isMyFriend($to);                     //$this->isFriend($to, $message);
 
-                if ($is_friend == 50000) {
-                    unset($auth);
-                    $this->sendRandom($to, $message);
-                }
-
-                if ($is_friend == 20000) {
+                if ($is_friend) {
                     $this->sendMessage(["login" => $from->login, "password" => $from->password], $to, $message);
+                    //dd("friend");
+                } else {
+                    $this->addContact($to, $message);
+                    
                 }
             }
             break;
         }
     }
 
-    public function checkValid()
-    {
+    public function checkValid() {
+        
     }
+
+    /* new part of code */
+
+    public function getContactsList() {
+        $req = json_decode($this->web("https://contacts.skype.com/contacts/v1/users/{$this->username}/contacts?\$filter=type%20eq%20%27skype%27%20or%20type%20eq%20%27msn%27%20or%20type%20eq%20%27pstn%27%20or%20type%20eq%20%27agent%27&reason=default"), true);
+
+        return isset($req["contacts"]) ? $req["contacts"] : [];
+    }
+
+    public function isMyFriend($skype_id) {
+        $friends = $this->getContactsList();
+        //dd($friends);
+        if (count($friends) != 0) {
+            foreach ($friends as $friend) {
+
+                if ($friend["id"] == $skype_id && $friend["authorized"] == "1") {
+
+
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public function addContact($to_username, $greeting = "Hello, I would like to add you to my contacts.") {
+
+
+        $to_username = $this->URLtoUser($to_username);
+
+        $post = [
+            "mri" => "8:" . $to_username,
+            "greeting" => $greeting
+        ];
+         
+        $req = json_decode($this->web("https://contacts.skype.com/contacts/v2/users/" . $this->username . "/contacts", "POST", json_encode($post)), true);
+        //dd($req["Message"]);
+        
+
+
+        return strlen($req["Message"])==0 ? true : false;
+    }
+
 }
