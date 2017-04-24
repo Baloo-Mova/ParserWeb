5603a5c4ae8ec1d69daa00427ed34d5ed58f8502
VlaD77 <ternovoyvladislav@mail.ru>
2017-04-24 20:16:53 +0300

    fix phone-numbers parser

--- app/Console/Commands/Parsers/ParseSite.php
+++ app/Console/Commands/Parsers/ParseSite.php
@@ -41,8 +41,9 @@ class ParseSite extends Command {
      * @return mixed
      */
     public function handle() {
-        $this->check = array("javascript:", "mailto:","/m/","/action_comment/",
-            ".tab",".?clear-cache", "#","#orderFlexTab", "tel:", "googtrans(", ".css", ".js",
+
+        $this->check = array("javascript:", "mailto:", "/m/", "/action_comment/",
+            ".tab", ".?clear-cache", "#", "#orderFlexTab", "tel:", "googtrans(", ".css", ".js",
             ".ico", ".jpg", ".png", ".jpeg", ".swf", ".gif");
         while (true) {
             $web = new Web();
@@ -50,26 +51,26 @@ class ParseSite extends Command {
 
             try {
                 $link = SiteLinks::join('tasks', 'tasks.id', '=', 'site_links.task_id')
-                        ->where(['site_links.reserved' => 0,'tasks.active_type' => 1])
-                        ->select('site_links.*')->first();
+                                ->where(['site_links.reserved' => 0, 'tasks.active_type' => 1])
+                                ->select('site_links.*')->first();
                 if (!isset($link)) {
                     sleep(10);
                     continue;
                 }
-               
-                
+
+
 
                 $link->reserved = 1;
                 $link->save();
-                
-                $search_queries = SearchQueries::where(['task_id'=>$link->task_id, 'link'=>$link->link])->first();
-                if(isset($search_queries)){
-                    $link->delete(); 
+
+                $search_queries = SearchQueries::where(['task_id' => $link->task_id, 'link' => $link->link])->first();
+                if (isset($search_queries)) {
+                    $link->delete();
                     //$link->reserved = 1;
                     // $link->save();
-                   continue;
+                    continue;
                 }
-                
+
                 $task_id = $link->task_id;
                 $default_link = $link->link;
 
@@ -80,70 +81,32 @@ class ParseSite extends Command {
                 $crawler->load($data);
 
                 $data = $crawler->find('body', 0);
-                  try
-                {
-                      $ff = $data->innertext;
-                      
-                  }
-                  catch(\Exception $ex){
-                    if(strpos($ex->getMessage(), "conv():")){
-                      $crawler = new SimpleHtmlDom(null, true, true, 'windows-1251', true, '\r\n', ' ');  
+                try {
+                    $ff = $data->innertext;
+                } catch (\Exception $ex) {
+                    if (strpos($ex->getMessage(), "conv():")) {
+                        $crawler = new SimpleHtmlDom(null, true, true, 'windows-1251', true, '\r\n', ' ');
                         $data = $web->get($link->link);
-               
+
                         $crawler->clear();
                         $crawler->load($data);
 
                         $data = $crawler->find('body', 0);
-                        
-                    } 
-                  }
-                  
-                  
+                    }
+                }
+
+
                 if (!empty($data)) {
 
                     $baseData = parse_url($link->link);
-                    
+
                     $emails = $this->extractEmails($data);
-                    
+
                     $phones = $this->extractPhones($data);
                     // dd($phones);
                     $skypes = $this->extractSkype($data);
-                   // dd($baseData);
-                    $additionalLinks = $this->extractlinks($data, $baseData);
-                    
-                   // dd($additionalLinks);
-                    
-                    if (count($additionalLinks) > 30) {
-                        array_splice($additionalLinks, 30, count($additionalLinks));
-                    }
-                    // print_r($additionalLinks);
+                    // dd($baseData);
 
-                    foreach ($additionalLinks as $l) {
-
-                     ////  echo($l . "\n");
-                        try {
-                            $data = $web->get($l);
-                            if (empty($data)) {
-                                continue;
-                            }
-                           
-                            $crawler->clear();
-                            $crawler->load($data);
-                            
-                            $data = $crawler->find('body', 0);
-                            
-                            
-                            $emails = $this->extractEmails($data, $emails);
-                          //  print_r($emails);
-                            $skypes = $this->extractSkype($data, $skypes);
-                            // print_r($skypes);
-                            $phones = $this->extractPhones($data);
-                            // print_r($skypes);
-                        } catch (\Exception $ex) {
-                           // dd($ex->getMessage());
-                        }
-                    }
-                   // dd($phones);
                     if (count($emails) > 0 || count($phones) > 0 || count($skypes) > 0) {
                         $res = new SearchQueries();
                         $res->mails = implode(',', $emails);
@@ -152,10 +115,56 @@ class ParseSite extends Command {
                         $res->link = $default_link;
                         $res->task_id = $task_id;
                         $res->save();
-                        
+                        $link->delete();
+                        continue;
                     }
-                    $link->delete();  
-                   
+
+
+                    $additionalLinks = $this->extractlinks($data, $baseData);
+
+                    // dd($additionalLinks);
+
+                    if (count($additionalLinks) > 30) {
+                        array_splice($additionalLinks, 30, count($additionalLinks));
+                    }
+                    // print_r($additionalLinks);
+
+                    foreach ($additionalLinks as $l) {
+
+                        ////  echo($l . "\n");
+                        try {
+                            $data = $web->get($l);
+                            if (empty($data)) {
+                                continue;
+                            }
+
+                            $crawler->clear();
+                            $crawler->load($data);
+
+                            $data = $crawler->find('body', 0);
+
+
+                            $emails = $this->extractEmails($data, $emails);
+                            //  print_r($emails);
+                            $skypes = $this->extractSkype($data, $skypes);
+                            // print_r($skypes);
+                            $phones = $this->extractPhones($data);
+                            // print_r($skypes);
+                        } catch (\Exception $ex) {
+                            // dd($ex->getMessage());
+                        }
+                    }
+                    // dd($phones);
+                    if (count($emails) > 0 || count($phones) > 0 || count($skypes) > 0) {
+                        $res = new SearchQueries();
+                        $res->mails = implode(',', $emails);
+                        $res->phones = implode(',', $phones);
+                        $res->skypes = implode(',', $skypes);
+                        $res->link = $default_link;
+                        $res->task_id = $task_id;
+                        $res->save();
+                    }
+                    $link->delete();
                 }
             } catch (\Exception $ex) {
                 $log = new ErrorLog();
@@ -168,10 +177,10 @@ class ParseSite extends Command {
 
     public function extractEmails($data, $before = []) {
         $plain = $data->plaintext;
-         
+
         //\Illuminate\Support\Facades\Storage::put("plain.txt", $plain);
         $html = $data->innertext;
-       //dd($html);
+        //dd($html);
         //preg_match_all("~[-a-z0-9_]+(?:\\.[-a-z0-9_]+)*@[-a-z0-9]+(?:\\.[-a-z0-9]+)*\\.[a-z]+~i", $plain, $emails);
         // $emails = array_unique($emails[0]);
         //dd($emails);
@@ -197,50 +206,66 @@ class ParseSite extends Command {
         return $before;
     }
 
+    private $ua_operators_code = array("039", "050", "066", "095", "099", "039", "067", "068", "096", "097", "098", "093", "091", "092", "094", "044");
+
     public function extractPhones($data, $before = []) {
-        $del = ["(", ")", " ", "-", "\t","#9658;","."];
+        $del = ["(", ")", " ", "-", "\t", "#9658;", "."];
         $plain = $data->plaintext;
         $plain = str_replace(["&nbsp;", "&larr;", "&rarr;"], [" ", ""], $plain);
-        
+
         //\Illuminate\Support\Facades\Storage::put("plain.txt", $plain);
 
         $html = $data->innertext;
-       // \Illuminate\Support\Facades\Storage::put("html.txt", $html);
+        // \Illuminate\Support\Facades\Storage::put("html.txt", $html);
         $plain = str_replace([" - ",], "-", $plain);
-        
+
         if (preg_match_all('/(\d{0,4})(?:\s?)\((\d{0,6})\)(?:\s?)([ |\-\d]{1,15})(?:\s?)(?:\-?)(?:\s?)([ \-\d]{1,11})?(?:\s?)/s', $plain, $M)) {
             $M = array_unique($M[0]);
             foreach ($M as $m) {
                 $m = str_replace($del, "", $m);
 
 
-                if (!in_array(trim($m), $before) && !$this->endsWith(trim($m), "png" && strlen($m) >= 9 && strlen($m) <= 12)
+                if (!in_array(trim($m), $before) && !$this->endsWith(trim($m), "png") && strlen($m) >= 9 && strlen($m) <= 12
                 ) {
-                    //   echo$m."\n";
-                    $before[] = trim($m);
-                }
-            }
-        }
-       // if (count($before) == 0) {
-          $plain =preg_replace("#(?<=\d)[\s-]+(?=\d)#","",$plain);
-            $plain = str_replace(["&#9658;","."], "", $plain);
-           // \Illuminate\Support\Facades\Storage::put("plain.txt", $plain);
-            if (preg_match_all('/(?:(\d{1,3})( |  ))?(?:([\(]?\d+[\)]?)[ -])?(\d{1,5}[\- ]?\d{1,5}[\- ]?\d{1,5})/s', $plain, $M)) {
-                $M = array_unique($M[0]);
-                foreach ($M as $m) {
-                    $m = str_replace($del, "", $m);
-//echo $m."\n";
+                  
+                    if (strlen($m) > 9 && ($m[0] != '2' || $m[0] != '1')) {
+                        if ($m[0] == '0') {
+                            foreach ($this->ua_operators_code as $i) {
+                                echo $i . "\n";
+                                if (strpos($m, $i) !== false) {
+                                    $m = "38" . $m;
+                                }
+                            }
+                        }
 
-                    if (!in_array(trim($m), $before) && !$this->endsWith(trim($m), "png" && (strlen($m) >= 9 && strlen($m) <= 12))
-                    ) {
-                        //   echo$m."\n";
-                        if(strlen($m)>10 && ($m[0]!='2'||$m[0]!='1'))
                         $before[] = trim($m);
+                       
                     }
                 }
             }
-       // }
-//dd($before);
+        }
+        // if (count($before) == 0) {
+        $plain = preg_replace("#(?<=\d)[\s-]+(?=\d)#", "", $plain);
+        $plain = str_replace(["&#9658;", "."], "", $plain);
+        // \Illuminate\Support\Facades\Storage::put("plain.txt", $plain);
+        if (preg_match_all('/(?:(\d{1,3})( |  ))?(?:([\(]?\d+[\)]?)[ -])?(\d{1,5}[\- ]?\d{1,5}[\- ]?\d{1,5})/s', $plain, $M)) {
+            $M = array_unique($M[0]);
+            foreach ($M as $m) {
+                $m = str_replace($del, "", $m);
+
+
+                if (!in_array(trim($m), $before) && !$this->endsWith(trim($m), "png") && strlen($m) >= 9 && strlen($m) <= 12
+                ) {
+                 //   echo$m . "\n";
+                    if ($m[0] == '7' || $m[0] == '8' || (strpos($m, "380") !== false && strpos($m, "380") == 0)) {
+                        $before[] = trim($m);
+                     //   echo"true11\n";
+                    }
+                }
+            }
+        }
+        // }
+        //dd($before);
         return $before;
     }
 
@@ -286,67 +311,65 @@ class ParseSite extends Command {
     public function extractlinks($data, $def_link, $before = []) {
 
         $html = $data->innertext;
-        
+
         $domain = "";
         //\Illuminate\Support\Facades\Storage::put("html.txt", $html);
 
         $protocol = $def_link["scheme"] . "://";
         $domain = $def_link["host"];
 
-       // dd($domain);
+        // dd($domain);
 
-          
+
         if (preg_match_all("/<a[^>]*href\s*=\s*'([^']*)'|" .
                         '<a[^>]*href\s*=\s*"([^"]*)"' . "/is", $html, $M)) {
 
             $M = array_unique($M[2]);
             //dd($M);
             foreach ($M as $m) {
-                 //dd($M);
+                //dd($M);
                 // echo($m."\n");
 //if doesn't start with http and is not empty
-                 if(!$this->validate($m)){continue;}
+                if (!$this->validate($m)) {
+                    continue;
+                }
                 if (strpos($m, "http") === false && trim($m) !== "") {
                     //checking if absolute path
-                     
+
                     if ($m == "/") {
                         $m = $protocol . $domain . "/";
-                          //dd($m."dfdfdsdsd");
+                        //dd($m."dfdfdsdsd");
                         //  echo($m . "\n");
                     } else {
-                        
+
                         if ($m[0] == '/') {
-                            
+
                             $m = substr($m, 1);
-                            
                         } else if ($m[0] == '.') {
-                           
+
                             while ($m[0] != '/') {
                                 $m = substr($m, 1);
                             }
                             $m = substr($m, 1);
-                           // echo("\n---".$m."\n");
-                          
+                            // echo("\n---".$m."\n");
                         }
-                        
+
                         $m = $protocol . $domain . "/" . $m;
-                        
-                        
                     }
                 }
-                
+
                 if (!in_array($m, $before) && trim($m) !== "") {
                     //if valid url
                     //if ($this->validate($m)) {
-                        //checking if it is url from our domain
-                        if (strpos($m, "http://" . $domain) === 0 || strpos($m, "https://" . $domain) === 0) {
-                            //adding url to sitemap array
-                            // $this->sitemap_urls[] = $url;
-                            //adding url to new link array
-                            
-                            $before[] = $m;
-                        }
-                   // }
+                    //checking if it is url from our domain
+                    if (strpos($m, "http://" . $domain) === 0 || strpos($m, "https://" . $domain) === 0) {
+                        //adding url to sitemap array
+                        // $this->sitemap_urls[] = $url;
+                        //adding url to new link array
+
+                        $before[] = $m;
+                    }
+                    // }
                 }
             }
         }
