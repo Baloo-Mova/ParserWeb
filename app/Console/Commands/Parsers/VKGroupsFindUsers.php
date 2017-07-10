81b9d7e806f642397e716474951be4c0977abf3c
Baloo-Mova <sergious-91@mail.ru>
2017-07-10 16:06:24 +0300

    qwe

--- app/Console/Commands/Parsers/VKGroupsFindUsers.php
+++ app/Console/Commands/Parsers/VKGroupsFindUsers.php
@@ -49,36 +49,43 @@ class VKGroupsFindUsers extends Command
     {
         while (true) {
             $this->content['vklink'] = null;
-            $mutex                   = new FlockMutex(fopen(__FILE__, "r"));
-            $mutex->synchronized(function () {
-                $group = VKLinks::join('tasks', 'tasks.id', '=', 'vk_links.task_id')->where([
-                    'vk_links.reserved' => 0,
-                    'vk_links.type'     => 0,
-                    'tasks.active_type' => 1,
-                ])->select('vk_links.*')->first();
-                if ( ! isset($group)) {
-                    return;
-                }
-
-                $group->reserved = 1;
-                $group->save();
-                $this->content['vklink'] = $group;
-            });
-
-            if ( ! isset($this->content['vklink'])) {
-                sleep(10);
-                continue;
-            }
-
             try {
-                $vk = new VK();
-                if ($vk->getUsersOfGroup($this->content['vklink'])) {
-                    $this->content['vklink']->delete();
+                $mutex = new FlockMutex(fopen(__FILE__, "r"));
+                $mutex->synchronized(function () {
+                    $group = VKLinks::join('tasks', 'tasks.id', '=', 'vk_links.task_id')->where([
+                        'vk_links.reserved' => 0,
+                        'vk_links.type'     => 0,
+                        'tasks.active_type' => 1,
+                    ])->select('vk_links.*')->first();
+                    if ( ! isset($group)) {
+                        return;
+                    }
+
+                    $group->reserved = 1;
+                    $group->save();
+                    $this->content['vklink'] = $group;
+                });
+
+                if ( ! isset($this->content['vklink'])) {
+                    sleep(10);
+                    continue;
                 }
-                sleep(rand(15, 40));
-            } catch (\Exception $ex) {
+
+                try {
+                    $vk = new VK();
+                    if ($vk->getUsersOfGroup($this->content['vklink'])) {
+                        $this->content['vklink']->delete();
+                    }
+                    sleep(rand(15, 40));
+                } catch (\Exception $ex) {
+                    $log          = new ErrorLog();
+                    $log->task_id = 0;
+                    $log->message = $ex->getMessage() . " line:" . $ex->getLine();
+                    $log->save();
+                }
+            }catch (\Exception $ex){
                 $log          = new ErrorLog();
-                $log->task_id = $this->content['vklink']->task_id;
+                $log->task_id = 0;
                 $log->message = $ex->getMessage() . " line:" . $ex->getLine();
                 $log->save();
             }
